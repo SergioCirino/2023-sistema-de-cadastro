5753b978910e0c3a60edab32c87c0bb19846969a
SergioCirino <110784486+SergioCirino@users.noreply.github.com>
2023-07-23 18:43:25 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aplicação esta se conectando com o Banco de Dados")
 console.log("mais uma alteração para teste")
 // alteração de teste 2
 console.log("eu precisaria de mais alguem para conseguir fazer isso direito")
+console.log("Subindo para o GITHub")
