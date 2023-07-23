383c3f6d1573bb23492e6859710264b61ce45247
SergioCirino <110784486+SergioCirino@users.noreply.github.com>
2023-07-23 18:37:04 -0300

    outro teste

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando com o Banco de Dados")
 console.log("mais uma alteração para teste")
+// alteração de teste 2
+console.log("eu precisaria de mais alguem para conseguir fazer isso direito")
