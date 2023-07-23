a9f760455f9269582a903077e0c9bc057e79b5c8
SergioCirino <110784486+SergioCirino@users.noreply.github.com>
2023-07-23 18:54:23 -0300

    voltanto atras, ultimo commit 5753b97

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando com o Banco de Dados")
-console.log("mais uma alteração para teste")
-// alteração de teste 2
-console.log("eu precisaria de mais alguem para conseguir fazer isso direito")
-console.log("Subindo para o GITHub")
